aa4fbf21bfaa38418f1101956abaf820194f7294
ErblinH <e.halabaku@buckaroo-kos.com>
2024-04-13 00:12:48 +0200

    Removed

--- Data2/Test.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Data;
-
-public class Test
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-}
--- GenerateCertificate/Test.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GenerateCertificate;
-
-public class Test
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-}
